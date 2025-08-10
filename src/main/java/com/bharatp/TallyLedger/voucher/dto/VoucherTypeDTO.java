a385e69f79e9c428d8177bfa795d6fda0d5b38a7
bharat2002 <bpaliwal70@gmail.com>
2025-08-10 21:58:53 +0530

    Partial Commit for Voucher Type DTO

--- src/main/java/com/bharatp/TallyLedger/voucher/dto/VoucherTypeDTO.java
+++ src/main/java/com/bharatp/TallyLedger/voucher/dto/VoucherTypeDTO.java
@@ -1,4 +1,68 @@
 package com.bharatp.TallyLedger.voucher.dto;
 
+import com.bharatp.TallyLedger.Company.entity.CompanyEntity;
+import com.bharatp.TallyLedger.voucher.util.VoucherTypeEnum;
+import jakarta.validation.constraints.NotNull;
+
 public class VoucherTypeDTO {
+    private Long id;
+    @NotNull
+    private VoucherTypeEnum name;
+
+    @NotNull
+    private boolean isCustom;
+
+    @NotNull
+    private Long companyId;
+
+    public VoucherTypeDTO(Long id, VoucherTypeEnum name, boolean isCustom, Long companyId, String description) {
+        this.id = id;
+        this.name = name;
+        this.isCustom = isCustom;
+        this.companyId = companyId;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public boolean isCustom() {
+        return isCustom;
+    }
+
+    public void setCustom(boolean custom) {
+        isCustom = custom;
+    }
+
+    public VoucherTypeEnum getName() {
+        return name;
+    }
+
+    public void setName(VoucherTypeEnum name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    private String description;
 }
--- src/main/java/com/bharatp/TallyLedger/voucher/mapper/VoucherTypeMapper.java
+++ src/main/java/com/bharatp/TallyLedger/voucher/mapper/VoucherTypeMapper.java
@@ -1,4 +1,21 @@
 package com.bharatp.TallyLedger.voucher.mapper;
 
-public interface VoucherTypeMapper {
+import com.bharatp.TallyLedger.voucher.dto.VoucherTypeDTO;
+import com.bharatp.TallyLedger.voucher.entity.VoucherTypeEntity;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface VoucherTypeMapper
+{
+    @Mappings(
+            {
+                    @Mapping(source ="", target ="")
+            }
+    )
+    VoucherTypeDTO toDTO(VoucherTypeDTO entity);
+
+
+    VoucherTypeEntity toEntity(VoucherTypeDTO dto);
+
+    void updateEntityFromDto(VoucherTypeDTO dto , @MappingTarget VoucherTypeEntity entity);
 }
